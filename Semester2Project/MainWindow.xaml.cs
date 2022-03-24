ff62db233bc7fe36e732fea80d32e0dea07d2352
MichealFallon <78159517+MichealFallon@users.noreply.github.com>
2022-03-24 10:59:52 +0000

    Add project files.

---
+++ Semester2Project/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Semester2Project
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<string> AllStructures = new List<string>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Material wood = new Material() { Name = "Wood" };
+            //Material stone = new Material() { Name = "Stone" };
+            //Material thatch = new Material() { Name = "Thatch" };
+            Structure sw = new Structure() { Name = "Stone Wall", word = "Derp" };
+            sw.Cost.Add("Wood", 1);
+
+            AllStructures.Add(sw.Name);
+            TextBlockCost.Text = "";
+            
+        }
+
+        private void ListBoxStructures_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Semester2Project/Structure.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semester2Project
+{
+    public class Material
+    {
+        public string Name { get; set; }
+    }
+    public class Structure
+    {
+        public string Name { get; set; }
+        public Dictionary<string , int> Cost { get; set; }
+        public string word { get; set; }
+        public Structure()
+        {
+            this.Cost = new Dictionary<string, int>();
+        }
+        public override string ToString()
+        {
+            return (word);
+        }
+        public string CostDisplay()
+        {
+            string costs = "";
+            foreach(var x in Cost)
+            {
+                costs = ("{0} x {1}", x.Value.ToString(), x.Key.ToString());
+            }
+            return costs;
+        }
+
+    }
+    //public class StoneWall
+    //{
+    //    public Dictionary<Material, int> Cost  { get; set; }
+        
+    //    public StoneWall()
+    //    {
+    //        this.Cost.Add(new Material() { Name = "Thatch" }, 15);
+    //        this.Cost.Add(new Material() { Name = "Stone" }, 40);
+    //        this.Cost.Add(new Material() { Name = "Wood" }, 20);
+    //    }
+
+    //}
+}
